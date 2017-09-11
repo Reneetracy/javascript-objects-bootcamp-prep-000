75ed92a5577e72b701c51e785dae3b597ce7c9ec
Renee Tracy <renee.upandaway@gmail.com>
2017-09-11 02:25:03 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {artistName, "songTitle"}
 
 function updatePlaylist (playlist, artistName, songTitle) {
-  object.assign ({}, playlist, { artistName: songTitle})
-  console.log(playlst)
+  object.assign ({}, 'artistName': "songTitle")
+  console.log(playlist)
 }
