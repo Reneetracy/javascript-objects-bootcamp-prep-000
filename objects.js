5d7e8a0b79f35a54d2f887d341359ff2e7695224
Renee Tracy <renee.upandaway@gmail.com>
2017-09-11 02:36:04 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,6 @@
-var playlist = new Object ({ artistName, "songTitle" });
+var playlist = {
+  Radiohead = "Weird Fishes/Arpeggi"
+  Bonobo = "El Toro"
+  Evenings = "Friend/Lover"
+  Shigeto = "Detroit Part 1"
+}
