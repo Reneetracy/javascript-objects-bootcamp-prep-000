bab9082398ab8ab6ee91460637e5e9dc4818adcb
Renee Tracy <renee.upandaway@gmail.com>
2017-09-11 02:15:00 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,8 @@
+var playlist = { artistName, "songTitle"}
+
+function updatePlaylist (playlist, artistName, songTitle) {
+  return object.assign ({}, playlist, { artistName: songTitle})
+}
+    
+
+
