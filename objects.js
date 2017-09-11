6bc2193c522d3d620b3f3f43c42e11ca5db933a0
Renee Tracy <renee.upandaway@gmail.com>
2017-09-11 03:31:50 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,5 +2,12 @@ var playlist = {
   Radiohead: 'Weird Fishes/Arpeggi',
   Bonobo: 'El Toro',
   Shigeto: 'Detroit Part 1',
-  ['Flying Lotus']: 'Coronus, the Terminator'
+  'Flying Lotus': 'Coronus, the Terminator'
 }
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = 'songTitle';
+  return playlist
+}
+
+function removeFromPlaylist()
