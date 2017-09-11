0e35fd596cb7bd2f21f514402650ce4d033a3fec
Renee Tracy <renee.upandaway@gmail.com>
2017-09-11 03:36:12 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,8 +6,7 @@ var playlist = {
 }
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist[artistName] = 'songTitle';
+  playlist[artistName] = songTitle;
   return playlist
 }
 
-function removeFromPlaylist()
